f9cd53a43abf7d553221e4b1b06060c4e258d214
Suman Kr. Maity <89718189+Sumanbit@users.noreply.github.com>
2026-02-13 17:49:48 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -109,7 +109,7 @@ export default function Footer({ onNavigate }: FooterProps) {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400 text-sm">
-          <p>&copy; {new Date().getFullYear()} SK TECHNOLOGY. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} SK TECHNOLOGY. All rights reserved. Designed by Suman kr. Maity </p>
         </div>
       </div>
     </footer>
